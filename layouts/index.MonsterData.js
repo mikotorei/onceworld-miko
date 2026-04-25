3d551fd6eed36f637c122f951567590133a550b3
mikotorei <leiaamaya0@gmail.com>
2026-04-25 19:16:57 +0900

    Update index.MonsterData.js

--- layouts/index.MonsterData.js
+++ layouts/index.MonsterData.js
@@ -9,19 +9,19 @@ window.MONSTERS = [
   attack_type: "{{ $m.Params.attack_type }}",
   attack_range: "{{ $m.Params.attack_range }}",
   level_shortcuts: {{ $m.Params.level_shortcuts | jsonify }},
-  exp: {{ $m.Params.exp }},
-  gold: {{ $m.Params.gold }},
-  capture_rate: {{ $m.Params.capture_rate }},
+  exp: {{ default 0 $m.Params.exp }},
+  gold: {{ default 0 $m.Params.gold }},
+  capture_rate: {{ default 0 $m.Params.capture_rate }},
   drops: {{ $m.Params.drops | jsonify }},
   locations: {{ $m.Params.locations | jsonify }},
-  vit: {{ $m.Params.status.vit }},
-  spd: {{ $m.Params.status.spd }},
-  atk: {{ $m.Params.status.atk }},
-  int: {{ $m.Params.status.int }},
-  def: {{ $m.Params.status.def }},
-  mdef: {{ $m.Params.status.mdef }},
-  luk: {{ $m.Params.status.luk }},
-  mov: {{ $m.Params.fixed_status.mov }}
+  vit: {{ default 0 $m.Params.status.vit }},
+  spd: {{ default 0 $m.Params.status.spd }},
+  atk: {{ default 0 $m.Params.status.atk }},
+  int: {{ default 0 $m.Params.status.int }},
+  def: {{ default 0 $m.Params.status.def }},
+  mdef: {{ default 0 $m.Params.status.mdef }},
+  luk: {{ default 0 $m.Params.status.luk }},
+  mov: {{ default 0 $m.Params.fixed_status.mov }}
 }{{ if lt (add $i 1) (len $sorted) }},{{ end }}
 {{- end }}
 ];
